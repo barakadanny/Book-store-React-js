f4a5b0403fe2ddfd5a1ab606b6902bef8e8ba1de
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-15 12:11:45 +0300

    Routting Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Categories from './components/Categories';
+import Books from './components/Books';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="Categories" element={<Categories />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
