818135f8a55dbfb4868b39e6972eeb843271bba9
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-15 17:56:54 +0300

    Nav bar links Style fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
-import { FaUser } from 'react-icons/fa';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { FaUser } from "react-icons/fa";
 
 function Header() {
+  const linkStyle = {
+    textDecoration: "none",
+    color: "#121212",
+  };
   return (
     <header>
       <nav>
         <h1>Bookstore CMS</h1>
         <ul className="nav-menu">
           <li>
-            <Link to="/">BOOKS</Link>
+            <Link style={linkStyle} to="/">
+              BOOKS
+            </Link>
           </li>
           <li>
-            <Link to="/Categories">CATEGORIES</Link>
+            <Link style={linkStyle} to="/Categories">
+              CATEGORIES
+            </Link>
           </li>
         </ul>
       </nav>
       <span>
-        {' '}
         <FaUser />
-        {' '}
       </span>
     </header>
   );
