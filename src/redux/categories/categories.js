2c4974dcf18c5c2a576a163de4e4fad79fe71a17
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-19 14:11:28 +0300

    Add Categories action and reducer for redux

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,17 @@
+const CHECK_STATUS = "book-store/src/redux/categories/CHECK_STATUS";
+
+const init = [];
+
+export default function categoriesReducer(state = init, action) {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
+
+export const checkStatus = (category) => ({
+  type: CHECK_STATUS,
+  payload: category,
+});
