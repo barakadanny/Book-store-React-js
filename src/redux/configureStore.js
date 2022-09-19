f34ff4ed6574513834c06265993a2028f7234292
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-19 14:20:04 +0300

    Refactor react store codes

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,10 @@
 import { combineReducers, configureStore } from "redux";
-
 import booksReducer from "./books/books";
 import categoriesReducer from "./categories/categories";
 
 const rootReducer = combineReducers({
-  books: booksReducer,
-  categories: categoriesReducer,
+  booksReducer,
+  categoriesReducer,
 });
 
 const store = configureStore({
