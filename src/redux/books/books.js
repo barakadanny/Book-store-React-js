568812834a0c91ca35178bdb1938564325a52f32
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-22 14:14:55 +0300

    POST data to APi

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,24 +1,27 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import key from "../FetchApi/apiKey";
+
 // redux actions
-const ADD_BOOK = 'book-store/src/redux/books/ADD_BOOK';
-const REMOVE_BOOK = 'book-store/src/redux/books/REMOVE_BOOK';
-const DISPLAY_BOOK = 'book-store/src/redux/books/DISPLAY_BOOKS';
+const ADD_BOOK = "book-store/src/redux/books/ADD_BOOK";
+const REMOVE_BOOK = "book-store/src/redux/books/REMOVE_BOOK";
+const DISPLAY_BOOK = "book-store/src/redux/books/DISPLAY_BOOKS";
 
 const init = {
   booksArr: [
     {
       id: 1,
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
+      title: "The Hunger Games",
+      author: "Suzanne Collins",
     },
     {
       id: 2,
-      title: 'Dune',
-      author: 'Frank Herbert',
+      title: "Dune",
+      author: "Frank Herbert",
     },
     {
       id: 3,
-      title: 'Capital in the Twenty-First Century',
-      author: 'Suzanne Collins',
+      title: "Capital in the Twenty-First Century",
+      author: "Suzanne Collins",
     },
   ],
 };
@@ -38,9 +41,18 @@ export default function booksReducer(state = init, action) {
 }
 
 // redux actions
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
+export const addBook = createAsyncThunk(ADD_BOOK, async (book) => {
+  await fetch(
+    `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/rRu8u7fR7sncSsKAbO3p/books`,
+    {
+      method: "POST",
+      body: JSON.stringify(book),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
+  return book;
 });
 
 export const removeBook = (book) => ({
